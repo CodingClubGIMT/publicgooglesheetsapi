0726b38f2889ae4a9689a57c1c34856bc517b677
geekodour <hrishikeshbman@gmail.com>
2017-08-11 00:29:24 +0530

    ok

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express')
 const utils = require('./utils.js')
 const app = express()
 
+app.set('port', (process.env.PORT || 5000));
+
 app.get('/', function (req, res) {
   res.json(['Hello World!'])
 })
@@ -19,6 +21,6 @@ app.get('/*', function (req, res) {
   res.json([])
 })
 
-app.listen(5000, function () {
-  console.log('Example app listening on port 5000!')
+app.listen(app.get('port'), function () {
+  console.log(`Example app listening on port ${app.get('port')}!`)
 })
